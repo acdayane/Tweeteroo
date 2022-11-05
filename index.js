66fa62f949f70a515414ea6897badb91e5b55eae
Dayane <acdayane@gmail.com>
2022-11-05 15:53:50 -0300

    10 tweets filtred

--- index.js
+++ index.js
@@ -7,28 +7,60 @@ app.use(cors());
 app.use(express.json());
 
 const users = [{
+  id: 1,
   username: 'bobesponja',
   avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
 }];
 
 const tweets = [{
+  id: 1,
   username: "bobesponja",
   tweet: "eu amo o hub"
 }];
 
 app.post('/sign-up', (req, res) => {
 
-  console.log(req.body)
-
   const newUser = {
+    id: users.length + 1,
     username: req.body.username,
     avatar: req.body.avatar
   }
 
-  users.push(newUser);
+  users.unshift(newUser);
 
   res.send("OK");
+
+  console.log(users)
+
 });
 
+app.post ('/tweets', (req, res) => {
+
+  const newTweet = {
+    id: tweets.length + 1,
+    username: req.body.username,
+    tweet: req.body.tweet,
+  }
+
+  tweets.unshift(newTweet);
+
+  res.send("OK");
+
+  console.log(tweets)
+
+});
+
+app.get ('/tweets', (req, res) => {
+
+  const tenTweets = [];
+
+  for (let i=0; i<10; i++){
+   tenTweets.push(tweets[i])
+  }
+
+  res.send(tenTweets)
+  console.log(tenTweets)
+
+});
 
 app.listen(5000, () => console.log('App is running in port 5000'));
